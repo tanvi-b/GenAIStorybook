879818b6e745c0cb4279756c0861b58fa080190f
tanvi-b <bhattadtanvi@gmail.com>
2025-03-30 12:03:19 -0500

    removed main

--- server.py
+++ server.py
@@ -60,5 +60,5 @@ def generate_image():
 def give_image(filename):
     return send_from_directory("images", filename)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8080)
